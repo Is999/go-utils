be06bd1de4110d6de225c64818a43ee6495c2567
Billy <>
2023-08-24 00:54:41 +0800

    golang V 1.21 帮助函数

--- runtime.go
+++ runtime.go
@@ -4,15 +4,15 @@ import (
 	"runtime"
 )
 
-type RuntimeInfo struct {
+type Frame struct {
 	Func string //方法名
 	File string //文件名
 	Line int    //行号
 }
 
-// GetRuntimeInfo 获取运行时行号、方法名、文件地址
-func GetRuntimeInfo(skip int) *RuntimeInfo {
-	info := new(RuntimeInfo)
+// RuntimeInfo 获取运行时行号、方法名、文件地址
+func RuntimeInfo(skip int) *Frame {
+	info := new(Frame)
 	pc, file, line, ok := runtime.Caller(skip)
 	if !ok {
 		info.File = "???"
