63123f06d0fa14e264fd0ba862aef97d00f014a4
Is999 <909931038@qq.com>
2023-08-31 12:49:21 +0800

    golang V 1.21 帮助函数

--- time.go
+++ time.go
@@ -24,47 +24,39 @@ func UTC() *time.Location {
 
 // patterns 时间格式化layout规则
 var patterns = strings.NewReplacer(
-	// ms us ns 必须放在最前面
 	"ms", "000", // 毫秒数
 	"us", "000", // 微秒数
 	"ns", "000", // 纳秒数
 
-	// 年
-	"Y", "2006", // 4 位数字完整表示的年份
+	"Y", "2006", // 4位数字完整表示的年份
 	"y", "06", // 2 位数字表示的年份
 
-	// 月
 	"m", "01", // 数字表示的月份，有前导零
 	"n", "1", // 数字表示的月份，没有前导零
 	"M", "Jan", // 三个字母缩写表示的月份
 	"F", "January", // 月份，完整的文本格式，例如 January 或者 March
 
-	// 日
-	"d", "02", // 月份中的第几天，有前导零的 2 位数字
-	"j", "2", // 月份中的第几天，没有前导零
+	"d", "02", // 月份中的第几天，有前导零的2位数字01到31
+	"j", "2", // 月份中的第几天，没有前导零，1到31
 
-	"D", "Mon", // 星期几，文本表示，3 个字母
-	"l", "Monday", // 星期几，完整的文本格式;L的小写字母
+	"D", "Mon", // 星期几，显示3个字母 Mon 到 Sun
+	"l", "Monday", // 星期几，完整的文本格式，Sunday 到 Saturday
 
-	// 时间
+	// 时间格式
 	"g", "3", // 小时，12 小时格式，没有前导零
-	"G", "15", // 小时，24 小时格式，没有前导零
 	"h", "03", // 小时，12 小时格式，有前导零
 	"H", "15", // 小时，24 小时格式，有前导零
 
 	"a", "pm", // 小写的上午和下午值
 	"A", "PM", // 大写的上午和下午值
 
-	"i", "04", // 有前导零的分钟数
+	"i", "04", // 分钟数，有前导零
 	"s", "05", // 秒数，有前导零
 
-	// time zone
-	"T", "MST", // 时区
+	// 时区
+	"e", "MST", // 时区标识
 	"P", "-07:00", // 时差
 	"O", "-0700", // 时差
-
-	// RFC 2822
-	"r", time.RFC1123Z,
 )
 
 // MonthDay 获取指定月份有多少天
@@ -111,12 +103,46 @@ func CheckDate(year, month, day int) bool {
 	return true
 }
 
-// DateInfo 获取日期信息
+// AddTime 获取日期信息
 //
 //	addTimes 增加时间（Y年，M月，D日，H时，I分，S秒，L毫秒，C微妙，N纳秒)，多个参数英文逗号分割
-//	unix Unix 时间sec秒和nsec纳秒
 //
 //	示例：DateInfo("-1D, 1M, -1Y, +4H, 20I, 30S, 76N",1299826006,152714700)
+func AddTime(t time.Time, addTimes ...string) (time.Time, error) {
+	for _, v := range addTimes {
+		v = strings.TrimSpace(v)
+		add, err := strconv.Atoi(strings.TrimSpace(v[:len(v)-1]))
+		if err != nil {
+			return t, errors.Wrap(err)
+		}
+
+		switch strings.ToUpper(v[len(v)-1:]) {
+		case "Y":
+			t = t.AddDate(add, 0, 0)
+		case "M":
+			t = t.AddDate(0, add, 0)
+		case "D":
+			t = t.AddDate(0, 0, add)
+		case "H":
+			t = t.Add(time.Hour * time.Duration(add))
+		case "I":
+			t = t.Add(time.Minute * time.Duration(add))
+		case "S":
+			t = t.Add(time.Second * time.Duration(add))
+		case "L":
+			t = t.Add(time.Millisecond * time.Duration(add))
+		case "C":
+			t = t.Add(time.Microsecond * time.Duration(add))
+		case "N":
+			t = t.Add(time.Nanosecond * time.Duration(add))
+		default:
+			return t, errors.New("addTimes 参数错误！")
+		}
+	}
+	return t, nil
+}
+
+// DateInfo 获取日期信息
 //
 //	返回：year int - 年，
 //		month int - 月，monthEn string - 英文月，
@@ -126,50 +152,8 @@ func CheckDate(year, month, day int) bool {
 //		unix int64 - 时间戳-秒，unixNano int64 - 时间戳-纳秒，
 //		weekDay int - 星期几，weekDayEn string - 星期几英文， yearWeek int - 一年中第几周，
 //		date string - 格式化日期，dateNs string - 格式化日期（纳秒)
-func DateInfo(addTimes string, unix ...int64) (map[string]interface{}, error) {
+func DateInfo(t time.Time) map[string]interface{} {
 	param := make(map[string]interface{})
-	t := time.Now()
-	if len(unix) == 1 {
-		var sec, nsec int64 = unix[0], 0
-		if sec >= 1e18 {
-			sec = unix[0] / 1e9
-			nsec = unix[0] % 1e9
-		}
-		t = time.Unix(sec, nsec)
-	} else if len(unix) > 1 {
-		t = time.Unix(unix[0], unix[1])
-	}
-	if addTimes != "" {
-		for _, v := range strings.Split(addTimes, ",") {
-			v = strings.TrimSpace(v)
-			add, err := strconv.Atoi(strings.TrimSpace(v[:len(v)-1]))
-			if err != nil {
-				return nil, errors.Wrap(err)
-			}
-			switch v[len(v)-1] {
-			case 'Y':
-				t = t.AddDate(add, 0, 0)
-			case 'M':
-				t = t.AddDate(0, add, 0)
-			case 'D':
-				t = t.AddDate(0, 0, add)
-			case 'H':
-				t = t.Add(time.Hour * time.Duration(add))
-			case 'I':
-				t = t.Add(time.Minute * time.Duration(add))
-			case 'S':
-				t = t.Add(time.Second * time.Duration(add))
-			case 'L':
-				t = t.Add(time.Millisecond * time.Duration(add))
-			case 'C':
-				t = t.Add(time.Microsecond * time.Duration(add))
-			case 'N':
-				t = t.Add(time.Nanosecond * time.Duration(add))
-			default:
-				return nil, errors.New("addTimes 参数错误！")
-			}
-		}
-	}
 
 	// 获取年
 	param["year"] = t.Year()
@@ -204,9 +188,11 @@ func DateInfo(addTimes string, unix ...int64) (map[string]interface{}, error) {
 	_, param["yearWeek"] = t.ISOWeek()
 	// 一年中的第几天
 	param["yearDay"] = t.YearDay()
+	// 格式化日期
 	param["date"] = t.Format(SecondDash)
+	// 格式化日期（纳秒)
 	param["dateNs"] = t.Format(NanosecondDash)
-	return param, nil
+	return param
 }
 
 // TimeFormat 时间格式化
@@ -265,79 +251,46 @@ func Date(timeZone *time.Location, layout string, unix ...int64) string {
 //	 - Strtotime(timeZone, "Y-m-d H:i:s", timestamp) // timestamp 时间
 func Strtotime(timeZone *time.Location, parse ...string) (t time.Time, err error) {
 	if len(parse) == 1 {
-		// 使用频率高地放前面
 		layouts := []string{
-			MonthDash,  // 2006-01
-			DayDash,    // 2006-01-02
-			HourDash,   // 2006-01-02 15
-			MinuteDash, // 2006-01-02 15:04
-			SecondDash, // 2006-01-02 15:04:05
-
-			Year,       // 2006
-			MonthSeam,  // 200601
-			DaySeam,    // 20060102
-			HourSeam,   // 2006010215
-			MinuteSeam, // 200601021504
-			SecondSeam, // 20060102150405
-
-			MonthSlash,  // 2006/01
-			DaySlash,    // 2006/01/02
-			HourSlash,   // 2006/01/02 15
-			MinuteSlash, // 2006/01/02 15:04
-			SecondSlash, // 2006/01/02 15:04:05
-
-			Month,  // 01
-			Day,    // 02
-			Hour,   // 15
-			Minute, // 04
-			Second, // 05
-
-			MillisecondDash,  // 2006-01-02 15:04:05.000
-			MicrosecondDash,  // 2006-01-02 15:04:05.000000
-			NanosecondDash,   // 2006-01-02 15:04:05.000000000
-			MillisecondSeam,  // 20060102150405.000
-			MicrosecondSeam,  // 20060102150405.000000
-			NanosecondSeam,   // 20060102150405.000000000
-			MillisecondSlash, // 2006/01/02 15:04:05.000
-			MicrosecondSlash, // 2006/01/02 15:04:05.000000
-			NanosecondSlash,  // 2006/01/02 15:04:05.000000000
-
-			SecondDash + " -07:00",      // 2006-01-02 15:04:05 -07:00
-			SecondSlash + " -07:00",     // 2006/01/02 15:04:05 -07:00
-			SecondDash + " -07:00 MST",  // 2006-01-02 15:04:05 -07:00 MST
-			SecondSlash + " -07:00 MST", // 2006/01/02 15:04:05 -07:00 MST
-
+			Year,                         // 2006
+			MonthDash,                    // 2006-01
+			DayDash,                      // 2006-01-02
+			HourDash,                     // 2006-01-02 15
+			MinuteDash,                   // 2006-01-02 15:04
+			SecondDash,                   // 2006-01-02 15:04:05
+			MonthSeam,                    // 200601
+			DaySeam,                      // 20060102
+			HourSeam,                     // 2006010215
+			MinuteSeam,                   // 200601021504
+			SecondSeam,                   // 20060102150405
+			MonthSlash,                   // 2006/01
+			DaySlash,                     // 2006/01/02
+			HourSlash,                    // 2006/01/02 15
+			MinuteSlash,                  // 2006/01/02 15:04
+			SecondSlash,                  // 2006/01/02 15:04:05
+			MillisecondDash,              // 2006-01-02 15:04:05.000
+			MicrosecondDash,              // 2006-01-02 15:04:05.000000
+			NanosecondDash,               // 2006-01-02 15:04:05.000000000
+			MillisecondSeam,              // 20060102150405.000
+			MicrosecondSeam,              // 20060102150405.000000
+			NanosecondSeam,               // 20060102150405.000000000
+			MillisecondSlash,             // 2006/01/02 15:04:05.000
+			MicrosecondSlash,             // 2006/01/02 15:04:05.000000
+			NanosecondSlash,              // 2006/01/02 15:04:05.000000000
+			SecondDash + " -07:00",       // 2006-01-02 15:04:05 -07:00
+			SecondSlash + " -07:00",      // 2006/01/02 15:04:05 -07:00
 			MillisecondDash + " -07:00",  // 2006-01-02 15:04:05.000 -07:00
 			MicrosecondDash + " -07:00",  // 2006-01-02 15:04:05.000000 -07:00
 			NanosecondDash + " -07:00",   // 2006-01-02 15:04:05.000000000 -07:00
 			MillisecondSlash + " -07:00", // 2006/01/02 15:04:05.000 -07:00
 			MicrosecondSlash + " -07:00", // 2006/01/02 15:04:05.000000 -07:00
 			NanosecondSlash + " -07:00",  // 2006/01/02 15:04:05.000000000 -07:00
-
-			MillisecondDash + " -07:00 MST",  // 2006-01-02 15:04:05.000 -07:00 MST
-			MicrosecondDash + " -07:00 MST",  // 2006-01-02 15:04:05.000000 -07:00 MST
-			NanosecondDash + " -07:00 MST",   // 2006-01-02 15:04:05.000000000 -07:00 MST
-			MillisecondSlash + " -07:00 MST", // 2006/01/02 15:04:05.000 -07:00 MST
-			MicrosecondSlash + " -07:00 MST", // 2006/01/02 15:04:05.000000 -07:00 MST
-			NanosecondSlash + " -07:00 MST",  // 2006/01/02 15:04:05.000000000 -07:00 MST
-
-			time.ANSIC,       // "Mon Jan _2 15:04:05 2006"
-			time.UnixDate,    // "Mon Jan _2 15:04:05 MST 2006"
-			time.RubyDate,    // "Mon Jan 02 15:04:05 -0700 2006"
-			time.RFC822,      // "02 Jan 06 15:04 MST"
-			time.RFC822Z,     // "02 Jan 06 15:04 -0700" // RFC822 with numeric zone
-			time.RFC850,      // "Monday, 02-Jan-06 15:04:05 MST"
-			time.RFC1123,     // "Mon, 02 Jan 2006 15:04:05 MST"
-			time.RFC1123Z,    // "Mon, 02 Jan 2006 15:04:05 -0700" // RFC1123 with numeric zone
-			time.RFC3339,     // "2006-01-02T15:04:05Z07:00"
-			time.RFC3339Nano, // "2006-01-02T15:04:05.999999999Z07:00"
-			time.Kitchen,     // "3:04PM"
-			time.Stamp,       // "Jan _2 15:04:05"
-			time.StampMilli,  // "Jan _2 15:04:05.000"
-			time.StampMicro,  // "Jan _2 15:04:05.000000"
-			time.StampNano,   // "Jan _2 15:04:05.000000000"
 		}
+
 		for _, layout := range layouts {
+			if len(layout) != len(parse[0]) {
+				continue
+			}
 			t, err = TimeParse(timeZone, layout, parse[0])
 			if err == nil {
 				return t, nil
